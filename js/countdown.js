dd9e27d0d602896c8783b2bd4a75a3c2fbe9f73c
willcoinproject <jalcos43@gmail.com>
2018-01-10 19:48:06 -0600

    updates

--- js/countdown.js
+++ js/countdown.js
@@ -17,7 +17,7 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  document.getElementById("countdown").innerHTML = "ICO Launch: " + days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text 
